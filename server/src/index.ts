13f2cddb225d6b18d54bb7c67a3b1cf9541b4a85
Carlos Bonetti <carlosb_bc@hotmail.com>
2020-01-08 21:56:23 -0300

    Boilerplate with initial express/typescript server

---
+++ server/src/index.ts
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.listen(port, () =>
+  console.log(`valid-chess-moves server listening on port ${port}!`)
+);
