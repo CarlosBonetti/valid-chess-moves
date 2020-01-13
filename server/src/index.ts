8e491acbc3fd35b8fbdc60e70ab126824da0282f
Carlos Bonetti <carlosbonetti.mail@gmail.com>
2020-01-13 20:35:04 -0300

    Add Heroku configuration

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,5 @@
 import { app } from "./server"
 
-const port = process.env.SERVER_PORT || 8000
+const port = process.env.PORT || 8000
 
 app.listen(port, () => console.log(`valid-chess-moves server listening on port ${port}!`))
