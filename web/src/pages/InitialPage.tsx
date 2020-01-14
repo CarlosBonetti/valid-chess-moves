86d60ee2581f1a94e5a696e9235a99bd2e45592d
Carlos Bonetti <carlosbonetti.mail@gmail.com>
2020-01-14 10:37:40 -0300

    Improve initial page

--- web/src/pages/InitialPage.tsx
+++ web/src/pages/InitialPage.tsx
@@ -6,24 +6,52 @@ import { ButtonLink } from "../components/ui"
 export function InitialPage() {
   return (
     <Container>
-      <h1>
-        Welcome to valid-chess-moves <Knight height="1.7em" />!
-      </h1>
-
       <p>
-        An interactive playground to show chess pieces valid moves <em>(currently supporting only the Knight)</em>.
+        <Knight height="6rem" />
       </p>
 
+      <h1>Welcome to valid-chess-moves!</h1>
+
       <p>
-        <ButtonLink to="/game">Start game</ButtonLink>
-        <ButtonLink to="/tour/1" primary>
-          Take the tour
+        An interactive playground to showcase chess pieces and their valid moves{" "}
+        <em>(currently supporting only the Knight)</em>.
+      </p>
+
+      <Divider />
+
+      <p>
+        <b>Take the tour</b> to see how this app works or <b>start the game</b> directly.
+      </p>
+
+      <Buttons>
+        <ButtonLink to="/tour/1">Take the tour</ButtonLink>
+        <ButtonLink to="/game" primary>
+          Start game
         </ButtonLink>
-      </p>
+      </Buttons>
     </Container>
   )
 }
 
 const Container = styled.div`
   max-width: 600px;
+  margin: 0 auto;
+  text-align: center;
+`
+
+const Divider = styled.hr`
+  border: none;
+  border-top: 1px solid #666;
+  margin: 2rem 0;
+`
+
+const Buttons = styled.div`
+  display: grid;
+  grid-gap: 1rem;
+  justify-content: center;
+  grid-template-columns: repeat(2, 220px);
+
+  @media only screen and (max-width: 600px) {
+    grid-template-columns: 1fr;
+  }
 `
